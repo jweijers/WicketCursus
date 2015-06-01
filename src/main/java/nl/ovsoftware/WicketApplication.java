7e5f16f363745736e65df6773904d5bd4e0e2442
Jeroen Weijers <jeroenweijers@gmail.com>
2015-06-01 19:01:21 +0200

    mount + cleanup

--- src/main/java/nl/ovsoftware/WicketApplication.java
+++ src/main/java/nl/ovsoftware/WicketApplication.java
@@ -6,8 +6,7 @@ import org.apache.wicket.protocol.http.WebApplication;
 /**
  * Application object for your web application.
  * If you want to run this application without deploying, run the Start class.
- * 
- * @see nl.ovsoftware.Start#main(String[])
+ *
  */
 public class WicketApplication extends WebApplication
 {
@@ -27,6 +26,7 @@ public class WicketApplication extends WebApplication
 	public void init()
 	{
 		super.init();
+		mountPage("/home", HomePage.class);
 
 		// add your configuration here
 	}
